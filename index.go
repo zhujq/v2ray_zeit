f8ea2d16502dfc80321ae5d2b85e1ee4e6bd9ab6
zhujq <14065567@qq.com>
2019-06-19 16:49:04 +0800

    v2ray

--- index.go
+++ index.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +13,27 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "SCHEME:"+r.URL.Scheme+"\r\n")
 	fmt.Fprintf(w, "METHOD:"+r.Method+"\r\n")
 	fmt.Fprintf(w, "HOST:"+r.Host+"\r\n")
+	if r.URL.Path == "/dw"{
+        remote, err := url.Parse("http://google.com")
+        if err != nil {
+                panic(err)
+        }
+
+        proxy := httputil.NewSingleHostReverseProxy(remote)
+        fmt.Fprintf(w, "Proxying...")
+        http.HandleFunc("/dw", handlerwww(proxy))
+        if err != nil {
+                panic(err)
+        }
+       
+
+	}
+}
+
+func handlerwww(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+        return func(w http.ResponseWriter, r *http.Request) {
+                log.Println(r.URL)
+                w.Header().Set("X-Ben", "Rad")
+                p.ServeHTTP(w, r)
+        }
 }
