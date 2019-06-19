8e80b60331247b6cd96ace1f97ee9bbf15a7db83
zhujq <14065567@qq.com>
2019-06-19 16:28:22 +0800

    v2ray

--- index.go
+++ index.go
@@ -8,7 +8,7 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!\r\n")
 	fmt.Fprintf(w, "PATH:"+r.URL.Path+"\r\n")
-	fmt.Fprintf(w, "SCHEME:"+r.Scheme+"\r\n")
+	fmt.Fprintf(w, "SCHEME:"+r.URL.Scheme+"\r\n")
 	fmt.Fprintf(w, "METHOD:"+r.Method+"\r\n")
 	fmt.Fprintf(w, "HOST:"+r.Host+"\r\n")
 
