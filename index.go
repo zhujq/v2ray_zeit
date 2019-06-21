3416f21bdd4517126e16e3484b540d08de5ce501
zhujq <14065567@qq.com>
2019-06-21 22:13:33 +0800

    v2ray

--- index.go
+++ index.go
@@ -7,15 +7,15 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Welcome you,your info:\r\n")
-	fmt.Println(w, "METHOD:"+r.Method+"\r\n")
-	fmt.Println(w, "URL:\r\n")
-	fmt.Println(w, "PATH:"+r.URL.Path+"\r\n")
-	fmt.Println(w, "SCHEME:"+r.URL.Scheme+"\r\n")
-	fmt.Println(w, "HOST:"+r.URL.Host+"URL-End\r\n")
-    fmt.Println(w, "Proto:"+r.Proto+"\r\n")
-	fmt.Println(w, "HOST:"+r.Host+"\r\n")
-	fmt.Println(w, "RequestUrl:"+r.RequestURI+"\r\n")
+	fmt.Fprintf(w, "Welcome you,your info:\r\n")
+	fmt.Fprintf(w, "METHOD:"+r.Method+"\r\n")
+	fmt.Fprintf(w, "URL:\r\n")
+	fmt.Fprintf(w, "PATH:"+r.URL.Path+"\r\n")
+	fmt.Fprintf(w, "SCHEME:"+r.URL.Scheme+"\r\n")
+	fmt.Fprintf(w, "HOST:"+r.URL.Host+"URL-End\r\n")
+    fmt.Fprintf(w, "Proto:"+r.Proto+"\r\n")
+	fmt.Fprintf(w, "HOST:"+r.Host+"\r\n")
+	fmt.Fprintf(w, "RequestUrl:"+r.RequestURI+"\r\n")
 	if r.URL.Path == "/dw"{
         resp, err := http.Get("http://www.google.com")
         if err != nil {
@@ -29,7 +29,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
           	panic(err)
     	}
  
-    	fmt.Println(string(body))    
+    	fmt.Fprintf(w,string(body))    
 
 	}
 }
