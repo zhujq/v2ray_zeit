f4b18f2ee551b0b978d28822689a618ee94a425e
zhujq <14065567@qq.com>
2019-06-22 22:14:52 +0800

    v2ray

--- index.go
+++ index.go
@@ -30,7 +30,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
     		if err != nil {
          		panic(err)
     		}
-   
+   			w.Header().Set("Content-Type", "text/plain")
             w.Write(body)
 
             if f, ok := w.(http.Flusher); ok {
