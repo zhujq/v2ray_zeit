bebb79619203ffab2ab1be23767606b7792911c8
zhujq <14065567@qq.com>
2019-06-24 14:18:50 +0800

    v2ray

--- index.go
+++ index.go
@@ -8,6 +8,8 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	var url, realhost string
+
 	switch r.URL.Path{
 		case "/":
 			fmt.Fprintf(w, "Welcome you,your info:\r\n")
@@ -19,170 +21,69 @@ func Handler(w http.ResponseWriter, r *http.Request) {
     		fmt.Fprintf(w, "Proto:"+r.Proto+"\r\n")
 			fmt.Fprintf(w, "HOST:"+r.Host+"\r\n")
 			fmt.Fprintf(w, "RequestUrl:"+r.RequestURI+"\r\n")
-		case "/google/":
-        	resp, err := http.Get("http://www.google.com")
-        	if err != nil {
-            	panic(err)
-        	}
+			return
 
-        	defer resp.Body.Close()
-        	
-        	body, err := ioutil.ReadAll(resp.Body)
-    		if err != nil {
-         		panic(err)
-    		}
-   		 	w.Header().Set("content-type", "text/html;charset=utf-8")
-            w.Write([]byte(body))
+		case "/google/":    //google入口
+			url = "http://www.google.com"
+			realhost = "www.google.com"
+			  
+    	case "/youtube/":   //youtube入口
+			url = "http://www.youtube.com"
+			realhost = "www.youtube.com"
+			    	
 
-          
-    		//	fmt.Fprintf(w,string(body))    
-    	case "/youtube/":
-        	resp, err := http.Get("http://www.youtube.com")
-        	if err != nil {
-            	panic(err)
-        	}
-
-        	defer resp.Body.Close()
-        	
-        	body, err := ioutil.ReadAll(resp.Body)
-    		if err != nil {
-         		panic(err)
-    		}
-   		 	w.Header().Set("content-type", "text/html;charset=utf-8")
-            w.Write([]byte(body))
-
-        case "/search":
-            resp, err := http.Get("http://www.google.com" + r.URL.String() )
-            if err != nil {
-            	panic(err)
-        	}
-
-        	defer resp.Body.Close()
-        	
-        	body, err := ioutil.ReadAll(resp.Body)
-    		if err != nil {
-         		panic(err)
-    		}
-   		// 	w.Header().Set("content-type", "text/html;charset=utf-8")
-            w.Write([]byte(body))	
-
-         case "/url":
-            resp, err := http.Get("http://www.google.com" + r.URL.String() )
-            if err != nil {
-            	panic(err)
-        	}
-
-        	defer resp.Body.Close()
-        	
-        	body, err := ioutil.ReadAll(resp.Body)
-    		if err != nil {
-         		panic(err)
-    		}
-   	//	 	w.Header().Set("content-type", "text/html;charset=utf-8")
-            w.Write([]byte(body))	
-
-        default:  // 
-         //	case "/images/":
+        default:    //  经google、youtube入口后重新返回的网址的处理，分离出真实主机名称 
     	 	var str string
     	 	str = r.URL.String()
-    //	 	fmt.Fprintf(w, "PATH:"+r.URL.Path+"\r\n")
-    	 	if strings.HasSuffix(str, "png"){
-    	 		str = "http://www.youtube.com" + str
-    	 		resp, err := http.Get(str)
-    	 		if err != nil {
-            	panic(err)
-        		}
-
-        		defer resp.Body.Close()
-        	
-        		body, err := ioutil.ReadAll(resp.Body)
-    			if err != nil {
-         			panic(err)
-    			}
-   		 		w.Header().Set("content-type", "image/png")
-            	w.Write([]byte(body))
-
-			}
-
-			if strings.HasSuffix(str, "css"){
-    	 		str = "http://www.youtube.com" + str
-    	 		resp, err := http.Get(str)
-    	 		if err != nil {
-            	panic(err)
-        		}
-
-        		defer resp.Body.Close()
-        	
-        		body, err := ioutil.ReadAll(resp.Body)
-    			if err != nil {
-         			panic(err)
-    			}
-   		 		w.Header().Set("content-type", "text/css")
-            	w.Write([]byte(body))
-
-            }
-
-            if strings.HasSuffix(str, "gif"){
-    	 		str = "http://www.youtube.com" + str
-    	 		resp, err := http.Get(str)
-    	 		if err != nil {
-            	panic(err)
-        		}
-
-        		defer resp.Body.Close()
-        	
-        		body, err := ioutil.ReadAll(resp.Body)
-    			if err != nil {
-         			panic(err)
-    			}
-   		 		w.Header().Set("content-type", "image/gif")
-            	w.Write([]byte(body))
-
-            }
-
-            if strings.HasSuffix(str, "js"){
-    	 		str = "http://www.youtube.com" + str
-    	 		resp, err := http.Get(str)
-    	 		if err != nil {
-            	panic(err)
-        		}
-
-        		defer resp.Body.Close()
-        	
-        		body, err := ioutil.ReadAll(resp.Body)
-    			if err != nil {
-         			panic(err)
-    			}
-   		 		w.Header().Set("content-type", "application/x-javascript")
-            	w.Write([]byte(body))
-
-            }
-
- 			if strings.HasSuffix(str, "jpg"){
-    	 		str = "http://www.youtube.com" + str
-    	 		resp, err := http.Get(str)
-    	 		if err != nil {
-            	panic(err)
-        		}
-
-        		defer resp.Body.Close()
-        	
-        		body, err := ioutil.ReadAll(resp.Body)
-    			if err != nil {
-         			panic(err)
-    			}
-   		 		w.Header().Set("content-type", "image/jpg")
-            	w.Write([]byte(body))
-
-            	}else {
-
-				fmt.Fprintf(w, "METHOD:"+r.Method+"\r\n")
-				fmt.Fprintf(w, "URL:\r\n")
-				fmt.Fprintf(w, "PATH:"+r.URL.Path+"\r\n")
-				fmt.Fprintf(w, "URL:"+r.URL.String()+"\r\n")
-
+			realhost = string([]byte(str)[:strings.Index(str,"/")])
+    	 	if realhost == ""{
+				fmt.Fprintf(w, "Failed to handle RequestUrl:"+str+"\r\n")
 			}
         	
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(r.Method, url, nil)
+	req.Header = r.Header
+	if err != nil {
+        panic(err)
+    }
+
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+
+    defer resp.Body.Close()
+        	
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+   		 //	w.Header().Set("content-type", "text/html;charset=utf-8")
+   	for k, _ := range resp.Header{
+   		w.Header().Set(k,resp.Header.Get(k))
+   	}
+	
+	if strings.Contains(string(resp.Header.Get("content-type")),"text/html"){
+
+		olds := "<a href=\"/"
+		news := "<a href=" + "\""+ "v2ray.14065567.now.sh/" + realhost + "/"
+		body = []byte(strings.ReplaceAll(string(body),olds,news))
+
+		olds = "src=\"/"
+		news = "<src=" + "\""+ "v2ray.14065567.now.sh/" + realhost+ "/"
+		body = []byte(strings.ReplaceAll(string(body),olds,news))
+
+		olds = "href=\"http://"
+		news = "href=" + "\""+ "v2ray.14065567.now.sh/" + "/"
+		body = []byte(strings.ReplaceAll(string(body),olds,news))
 
 	}
+			
+			
+    w.Write([]byte(body))
+
+          
+    //	fmt.Fprintf(w,string(body))  
 }
